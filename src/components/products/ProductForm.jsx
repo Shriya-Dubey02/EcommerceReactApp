d17114a668d28f2eb8f618deb87e5c2398c19edc
Shriya-Dubey02 <shriyadubey220@gmail.com>
2025-01-15 23:54:34 +0530

    15 Jan

--- src/components/products/ProductForm.jsx
+++ src/components/products/ProductForm.jsx
@@ -1,33 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { addProduct } from '../../services/ProductService'
 
 function ProductForm() {
+  let[searchQuery,setSearchQuery]=useState("");
+  const searchFunction=(e)=>{
+    setSearchQuery(e.target.value);
+  
+  }
+
+// function to be called when form will be submitting
+
+const submitHandler=(e)=>{
+
+  addProduct({
+    productId: e.target.productId.value,
+    productName: e.target.productName.value,
+    productDescription: e.target.productDescription.value,
+    productPrice: e.target.productPrice.value
+
+  }).then(data=>data)
+}
+
+
+// ============================================================
+
+
   return (
     <div className="container border border-danger border-3 p-3 my-6">
       
-      <form>
+      <input type="text" onChange={searchFunction} />
+
+
+
+      <form onSubmit={submitHandler}>
         <h1 className='bg-danger p-3 text-white text-center'>Add Product</h1>
         {/* Product Id */}
       <div className="mb-3">
     <label for="exampleInputEmail1" className="form-label" >Product Id</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="productId"/>
+    <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="productId"/>
   </div>
 
    {/* Product name */}
    <div className="mb-3">
     <label for="exampleInputEmail1" className="form-label" >Product name</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="productName"/>
+    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="productName"/>
   </div>
 
    {/* Product Description */}
    <div className="mb-3">
     <label for="exampleInputEmail1" className="form-label" >Product Description</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="productDescription"/>
+    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="productDescription"/>
   </div>
 
    {/* Product Price */}
    <div className="mb-3">
     <label for="exampleInputEmail1" className="form-label" >Product Price</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="productPrice"/>
+    <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="productPrice"/>
   </div>
 
 {/* Button to submit form */}
