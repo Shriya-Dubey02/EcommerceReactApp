c7e4fd38d23d4fa0ffdd68dd4d05a1be4ec90cc5
Shriya-Dubey02 <shriyadubey220@gmail.com>
2025-01-27 16:14:13 +0530

    27 jan

--- src/components/products/ProductItem.jsx
+++ src/components/products/ProductItem.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { deleteProduct, getProductsById } from '../../services/ProductService'
+import { getCategories } from '../../services/CategoryService'
 
 
 // we will be creating props 
 function ProductItem({productName,productDescription,productPrice,product_link,onSelectProduct,onDeleteProduct}) {
+
+  let[category,setCategory]=useState([])
+
+  // const fetchCategories=async ()=>{
+  //   setCategory(await getCategories())
+    
+  // }
+
+  // useEffect(()=>{
+  //   fetchCategories();
+
+  // },[])
+
+  useEffect(()=>{
+    getCategories().then(data=>{
+      setCategory(data)
+    })
+  },[])
+
+  // ======================================================
 // Updating product
   const onSelectUpdate=async (link)=>{
   //  console.log(link);
@@ -32,6 +53,33 @@ const onSelectDelete=async (product_id_link)=>{
         <h5 class="card-title">{productName}</h5>
         <p class="card-text">{productDescription}</p>
         <p class="card-text">Price: {productPrice}</p>
+        
+        {/* to add categories for product: 27 Jan */}
+        {/* Showing categories */}
+        <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
+    Select Category
+  </button>
+  
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+  
+    {category.map((cat)=>{
+      
+
+      return(
+        <li><button class="dropdown-item" type="button">Act</button></li>
+
+      )
+    })}
+  </ul>
+</div>
+
+
+
+
+      {/* ================================================ */}
+
+
      
         {/* Update Button */}
         <button className='btn btn-success' onClick={()=>{onSelectUpdate(product_link)}}>Update</button>
---
+++ src/services/CategoryService.jsx
@@ -0,0 +1,11 @@
+import { data } from "react-router-dom"
+
+const API_LINK="http://localhost:8080/category"
+
+export const getCategories=()=>{
+
+    return fetch(API_LINK)
+    .then(data=>data.json())
+    .then(data=>data["_embedded"]["categories"])
+
+}
